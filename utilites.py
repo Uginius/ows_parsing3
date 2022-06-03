860f360930f04de49a2a118b7df0b46933e5459c
Uginius <e.slastin@gmail.com>
2022-06-03 15:06:42 +0300

    add write_html function

--- utilites.py
+++ utilites.py
@@ -7,8 +7,12 @@ def time_track(func):
         result = func(*args, **kwargs)
         ended_at = time.time()
         elapsed = round(ended_at - started_at, 4)
-        print(f'\nФункция работала {elapsed} секунд(ы), или {elapsed//60} минут.')
+        print(f'\nФункция работала {elapsed} секунд(ы), или {elapsed // 60} минут.')
         return result
 
     return surrogate
 
+
+def write_html(src, filename):
+    with open(filename, 'w', encoding='utf8') as write_file:
+        write_file.write(src)
