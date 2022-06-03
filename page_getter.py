59b754f5df163699d402642a5272e3a1b656dc75
Uginius <e.slastin@gmail.com>
2022-06-03 21:32:47 +0300

    page getter works

--- page_getter.py
+++ page_getter.py
@@ -1,10 +1,12 @@
 import os
+import time
 from datetime import datetime
 from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from config import selenium_arguments, browser_path
+from config import selenium_arguments, browser_path, wait_time
 from src.goods_ids import oz_links, wb_links
+from utilites import write_html
 
 
 class PageGetter(Thread):
@@ -15,12 +17,11 @@ class PageGetter(Thread):
         self.platform = None
         self.links = {}
         self.browser = None
-        self.cur_url = None
 
     def run(self):
         self.check_html_dir()
-        # self.initiate_browser()
-        self.search_cycle()
+        self.initiate_browser()
+        self.get_pages_cycle()
         if self.browser:
             self.browser.close()
 
@@ -34,12 +35,31 @@ class PageGetter(Thread):
         options.add_argument(selenium_arguments[1])
         self.browser = webdriver.Chrome(service=Service(executable_path=browser_path), options=options)
 
-    def search_cycle(self):
+    def get_pages_cycle(self):
         ln = len(self.links)
         for n, art in enumerate(self.links):
-            self.cur_url = self.links[art]
+            cur_url = self.links[art]
             order = n + 1
-            print(f'{self.platform}, №{order:04}/{ln}, art: {art}, connecting to {self.cur_url}')
+            print(f'{self.platform}, №{order:04}/{ln}, art: {art}, connecting to {cur_url}')
+            self.browser.get(url=cur_url)
+            self.scroll_down()
+            # time.sleep(wait_time)
+            write_html(self.browser.page_source, f'{self.html_dir}{self.date}_N{order:04}_{self.platform}_{art}.html')
+
+    def scroll_down(self):
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+        self.browser.execute_script(f"window.scrollTo(0, {last_height});")
+        sl_time = 0.5
+        time.sleep(sl_time)
+        while True:
+            self.browser.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            time.sleep(sl_time)
+            if new_height == last_height:
+                break
+            last_height = new_height
+        self.browser.execute_script(f"window.scrollTo(0, {new_height - 2000});")
+        time.sleep(sl_time * 3)
 
 
 class GetterOz(PageGetter):
@@ -48,6 +68,25 @@ class GetterOz(PageGetter):
         self.html_dir = f'htmls/{self.date}/oz_html_files/'
         self.platform = 'oz'
         self.links = oz_links
+
+    def scroll_down(self):
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+        self.browser.execute_script(f"window.scrollTo(0, {last_height});")
+        sl_time = 0.5
+        time.sleep(sl_time)
+        for i in range(1, 5):
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            self.browser.execute_script(f"window.scrollTo(0, {new_height - 500 * i});")
+            time.sleep(sl_time)
+            self.browser.execute_script(f"window.scrollTo(0, {new_height + 500 * i});")
+            time.sleep(sl_time)
+        while True:
+            self.browser.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            time.sleep(sl_time * 3)
+            if new_height == last_height:
+                break
+            last_height = new_height
 
 
 class GetterWb(PageGetter):
