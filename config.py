0e6b6007b68b471e40389ac4d33acc526241c62f
eugeneslastin <e.slastin@gmail.com>
2022-07-04 09:11:25 +0300

    no working correctly

--- config.py
+++ config.py
@@ -1,19 +1,22 @@
 import sys
 
-selenium_arguments = [
-    'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
-    'Chrome/97.0.4692.99 Safari/537.36',
-    '--disable-blink-features=AutomationControlled']
 
+wait_time = 5
 
 match sys.platform:
     case 'linux':
-        browser_path = 'drivers/'
+        browser_path = 'drivers/chromedriver_linux64_99.0.4844.51/chromedriver'
+        user_agent = None
     case 'darwin':
         browser_path = 'drivers/chromedriver'
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     case 'win32':
         browser_path = 'drivers/chromedriver.exe'
+        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
     case _:
         print("ERROR: can't found selenium driver")
+        user_agent = None
 
-wait_time = 5
+selenium_arguments = [f'user-agent={user_agent}', '--disable-blink-features=AutomationControlled']
+req_headers = {'accept': '*/*', 'accept-encoding': 'gzip, deflate, br',
+               'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7', 'user-agent': user_agent}
--- page_getter.py
+++ page_getter.py
@@ -5,7 +5,7 @@ from random import randint
 from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from config import selenium_arguments, browser_path, wait_time
+from config import selenium_arguments, browser_path
 from src.goods_ids import oz_links, wb_links
 from utilites import write_html
 
@@ -41,12 +41,18 @@ class PageGetter(Thread):
         for n, art in enumerate(self.links):
             cur_url = self.links[art]
             order = n + 1
-            print(f'{self.platform}, №{order:04}/{ln}, art: {art}, connecting to {cur_url}')
+            print(f'{self.platform}, №{order:04}/{ln:04}, art: {art}, connecting to {cur_url}')
             self.browser.get(url=cur_url)
             self.scroll_down()
-            time.sleep(wait_time)
+            time.sleep(randint(5, 15))
+            self.scroll_up()
+            self.scroll_down()
             data = self.browser.page_source
             write_html(data, f'{self.html_dir}N{order:04}_{self.platform}_{art}.html')
+
+    def scroll_up(self):
+        self.browser.execute_script(f"window.scrollTo(0, 0);")
+        time.sleep(1)
 
     def scroll_down(self):
         last_height = self.browser.execute_script("return document.body.scrollHeight")
@@ -72,25 +78,26 @@ class GetterOz(PageGetter):
         self.links = oz_links
 
     def scroll_down(self):
-        # last_height = self.browser.execute_script("return document.body.scrollHeight")
-        # self.browser.execute_script(f"window.scrollTo(0, {last_height});")
-        # sl_time = 0.5
-        # time.sleep(sl_time)
-        # for i in range(1, 5):
-        #     new_height = self.browser.execute_script("return document.body.scrollHeight")
-        #     self.browser.execute_script(f"window.scrollTo(0, {new_height - 1000 * i});")
-        #     time.sleep(sl_time * 3)
-        # while True:
-        #     self.browser.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
-        #     new_height = self.browser.execute_script("return document.body.scrollHeight")
-        #     time.sleep(sl_time * 3)
-        #     if new_height == last_height:
-        #         break
-        #     last_height = new_height
-        for _ in range(3):
+        last_height = self.browser.execute_script("return document.body.scrollHeight")
+        self.browser.execute_script(f"window.scrollTo(0, {last_height});")
+        sl_time = 0.5
+        time.sleep(sl_time)
+        for i in range(1, 5):
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            self.browser.execute_script(f"window.scrollTo(0, {new_height - 1000 * i});")
+            time.sleep(sl_time * 3)
+        while True:
+            self.browser.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
+            new_height = self.browser.execute_script("return document.body.scrollHeight")
+            time.sleep(sl_time * 3)
+            if new_height == last_height:
+                break
+            last_height = new_height
+        for i in range(3):
             sleep_time = randint(2, 5)
             time.sleep(sleep_time)
-            self.browser.execute_script("window.scrollBy(0, 1500)")
+            value = (i + 1) * 1500
+            self.browser.execute_script(f"window.scrollBy(0, {value})")
 
 
 class GetterWb(PageGetter):
